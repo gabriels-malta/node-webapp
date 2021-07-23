c4388f14b12df2998caa42bf747ea562443974b0
Gabriel Malta <gabriel.malta@coxautoinc.com>
2021-07-23 18:40:58 -0300

    serving a html page

--- app/src/index.js
+++ app/src/index.js
@@ -2,14 +2,15 @@ const express = require('express'),
     chalk = require('chalk'),
     debug = require('debug')('server:index'),
     morgan = require('morgan'),
+    path = require('path'),
     SERVER_PORT = 4242;
 
 const server = express();
 
 server.use(morgan('tiny'));
+server.use(express.static(path.join(__dirname, '/public/')));
 
 server.get('/', (req, res) => {
-    res.statusCode = 200;
     res.send('It works!');
 });
 
